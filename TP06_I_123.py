d9884c0c6726540ba761501c8f2b9d00311a8780
Carci Enzo <carci.enzo763@gmail.com>
2026-03-12 13:11:05 +1100

    TP06

---
+++ TP06_I_123.py
@@ -0,0 +1,22 @@
+#exercice 1
+""""
+def age_verification():
+    age = int(input("Quel est votre âge ? Ton âge est : "))
+    if age >= 18 :
+        print("Vous êtes majeur, vous pouvez tuer votre foie ! ")
+    else :
+        print("Tu es mineur, que du jus pour toi... Mais on dit aussi 'pas vue pas pris' °^ ")
+age_verification()
+
+#exercice 2
+def type_de_nombres() : 
+    nombre = int(input("Ecris ton nombres : "))
+    if nombre > 0 :
+        print("Ton nombres est positif ")
+    elif nombre == 0 :
+        print("Ton nombres est égal à zéros ")
+    else :
+        print("Ton nombres est négatif ")
+type_de_nombres() 
+"""
+#exercice 3 
